8f020b0595217661a1bf33720f54ed9f1507b8c8
BelohvostovAlex <belohvostov.belohvostov@gmail.com>
2021-10-07 11:49:57 +0300

    added converter

--- js/app.js
+++ js/app.js
@@ -7,19 +7,16 @@ document.addEventListener('DOMContentLoaded', () => {
     function hide(collection) {
         collection.forEach(item => {
             item.classList.add('hide', 'tabAnime')
-        })
-    }
-    hide(tabs)
-
-    function show (num = 0) {
-        hide(tabs)
-        tabs.forEach(item => {
             item.classList.remove('show')
         })
-        tabs[num].classList.add('show')
     }
 
-    show()
+    function show (num = 0, elem) {
+        hide(elem)
+        elem[num].classList.add('show')
+    }
+
+    show(0,tabs)
 
     tabItems.forEach(tabItem => {
         tabItem.addEventListener('click', (e) => {
@@ -27,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 item.classList.remove('tabheader__item_active')
             })
         e.target.classList.add('tabheader__item_active')
-        show(Array.from(tabItems).indexOf(e.target))
+        show(Array.from(tabItems).indexOf(e.target), tabs)
         })
     })
 
@@ -119,31 +116,70 @@ document.addEventListener('DOMContentLoaded', () => {
     //slider
     const allSlides = document.querySelectorAll('.offer__slide'),
         prevSlide = document.querySelector('.offer__slider-prev'),
-        nextSlide = document.querySelector('.offer__slider-next')
-
-    function showSlides(i=1) {
-        let totalSlides = document.querySelector('#total'),
-            currentSlide = document.querySelector('#current')
-        allSlides.forEach(slide => {
-            slide.classList.add('hide')
-            slide.classList.remove('show')
-        })
-        allSlides[i].classList.add('show')
-        if(allSlides.length < 10 ) {
-            totalSlides.innerHTML = '0' + allSlides.length
-        } else {
-            totalSlides.innerHTML = allSlides.length
-        } if(i + 1 < 10) {
-            currentSlide.innerHTML = '0' + (i + 1)
-        } else {
-            currentSlide.innerHTML = i + 1
-        }
-            
+        nextSlide = document.querySelector('.offer__slider-next'),
+        currentSlide = document.querySelector('#current'),
+        totalSlides = document.querySelector('#total')
+      
+    function showSlide (num = 0, collection) {
+        show(num,collection)
     }
-    showSlides()
-    nextSlide.addEventListener('click', () => {
-        showSlides(i+1)
-    })
+
+    showSlide(0,allSlides)
 
 
+        
+
+
+
+//Menu Items + ConverterAPI
+    class menuItem {
+        constructor(src,alt,title,descr,price,fromCurr,toCurr,parentSelector) {
+            this.src = src
+            this.alt = alt
+            this.title = title
+            this.descr = descr
+            this.price = price
+            this.fromCurr = fromCurr
+            this.toCurr = toCurr
+            this.parentSelector = parentSelector
+            this.url = `https://v6.exchangerate-api.com/v6/4e2b4d783c66b845da4bd609/latest/${this.fromCurr}`
+        }
+
+        async converter () {
+            let response = await fetch(this.url)
+            let json = await response.json()
+            console.log(json)
+            let byn = json.conversion_rates[this.toCurr]
+            let convertedPrice = this.price * byn
+            console.log(convertedPrice)
+            this.price = convertedPrice.toFixed(2)
+            // console.log(this)
+            return new Promise(resolve => resolve(this))
+        }
+
+        createItem() {
+            let div = document.createElement('div')
+                div.innerHTML = `
+                <div class="menu__item">
+                <img src=${this.src} alt=${this.post}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> ${this.toCurr}/день</div>
+                </div>
+            </div>
+                `
+                document.querySelector('.menu__field .container').append(div)
+        }
+    }
+
+    new menuItem('img/tabs/elite.jpg', 'elite', 'Меню “Премиум”', 'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!', 5,'USD', 'BYN', '.menu__field .container').converter().then(result => result.createItem())
+    new menuItem('img/tabs/vegy.jpg', 'vegy', 'Меню "Фитнес"', 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 4,'USD', 'BYN', '.menu__field .container').converter().then(result => result.createItem())
+    new menuItem('img/tabs/post.jpg', 'post', 'Меню "Постное"', 'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.', 2,'USD', 'BYN', '.menu__field .container').converter().then(result => result.createItem())
+    
+
+    
+ 
 })
